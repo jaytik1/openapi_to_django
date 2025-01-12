0f9aa5d0db3fac870d991f67f9835a668f22d6fd
jaytik1 <67837855+jaytik1@users.noreply.github.com>
2025-01-12 11:39:42 +0000

    feat: user can specify a filepath and that it should be parsed as YAML
    Refs: #4

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
+import argparse
+import os
+
+
+YAML_EXTENSIONS = ["yaml", "yml"]
+
+
 def main() -> None:
-    print("OpenAPI to Django")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("filepath", help="file path of the OpenAPI document")
+    parser.add_argument(
+        "-y",
+        "--parse-yaml",
+        help="specify that the given file should be parsed as YAML",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+
+    if args.parse_yaml:
+        parse_yaml(args.filepath)
+    elif os.path.splitext(args.filepath)[1] in YAML_EXTENSIONS:
+        # parse file as YAML if the given file has a YAML extension
+        parse_yaml(args.filepath)
+    else:
+        print("File type not determined, use -y to parse as YAML.")
+
+
+def parse_yaml(filepath: str) -> None:
+    print(filepath)
 
 
 if __name__ == "__main__":
