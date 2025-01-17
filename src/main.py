d4f7451a70e147f0f4ac210a6eec8e3d1a9439af
jaytik1 <67837855+jaytik1@users.noreply.github.com>
2025-01-17 10:49:35 +0000

    feat: write a JSON file from a Python dictionary or list
    Refs: #8

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import argparse
+import datetime
+import json
 import os
 import yaml
+from pathlib import Path
 from typing import Any, Mapping
 
 
+INDENT_SPACES = 2
 YAML_EXTENSIONS = [".yaml", ".yml"]
 
 
@@ -29,6 +33,7 @@ def main() -> None:
         print("File type not determined, use -y to parse as YAML.")
         return
 
+    write_json(openapi)
     print(openapi)
 
 
@@ -50,6 +55,51 @@ def parse_yaml(filepath: str) -> Mapping[str, Any]:
                 print(exc)
     except FileNotFoundError:
         print(f"File {filepath} not found.")
+
+
+# TODO make a more precise data type definition
+def write_json(data: Mapping[str, Any], filepath: str = "file.json") -> None:
+    """Take an object, convert it to JSON and save it to a JSON file.
+
+    Args:
+        data: Python dictionary or list data to be saved as JSON.
+        filepath: Path of the JSON file that should be written.
+
+    Raises:
+        OSError: Attempted to overwrite an existing file.
+        TypeError: Given data is not JSON serialisable.
+    """
+    filepath_object = Path(filepath)
+
+    if filepath_object.exists():
+        raise OSError(f"File already exists at given filepath: {filepath}.")
+
+    with open(filepath_object, "w") as json_file:
+        try:
+            json.dump(data, json_file, indent=INDENT_SPACES, default=json_handle_value)
+        except TypeError as e:
+            # delete partially-written file if there is an error
+            filepath_object.unlink()
+            raise TypeError(e)
+
+
+def json_handle_value(value: Any):
+    """Attempt to convert a non-serialisable value into a JSON serialisable one.
+
+    Args:
+        value: Non-serialisable value to be converted.
+
+    Raises:
+        TypeError: Given value is not possible to be JSON serialisable.
+
+    Returns:
+        Value in a JSON serialisable form.
+    """
+    if isinstance(value, datetime.date) or isinstance(value, datetime.datetime):
+        # converts date and datetime objects to an ISO 8601 string
+        return value.isoformat()
+    else:
+        raise TypeError(f"Value {value} is not JSON serialisable.")
 
 
 if __name__ == "__main__":
