b5556ca8db776be8fd8acf4b4d755ecbed8593d0
jaytik1 <67837855+jaytik1@users.noreply.github.com>
2025-01-17 10:48:10 +0000

    feat: parse YAML files (#10)
    * docs: added instructions for setting up a virtual environment to README.md
    
    * chore: install PyYAML and added it to requirements.txt
    
    * feat: user can specify a filepath and that it should be parsed as YAML
    
    * fix: fixed YAML extensions to include the "."
    
    * feat: created a function to parse a YAML file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,55 @@
+import argparse
+import os
+import yaml
+from typing import Any, Mapping
+
+
+YAML_EXTENSIONS = [".yaml", ".yml"]
+
+
 def main() -> None:
-    print("OpenAPI to Django")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("filepath", help="file path of the OpenAPI document")
+    parser.add_argument(
+        "-y",
+        "--parse-yaml",
+        help="specify that the given file should be parsed as YAML",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+
+    if args.parse_yaml:
+        openapi = parse_yaml(args.filepath)
+    elif os.path.splitext(args.filepath)[1] in YAML_EXTENSIONS:
+        # parse file as YAML if the given file has a YAML extension
+        openapi = parse_yaml(args.filepath)
+    else:
+        print("File type not determined, use -y to parse as YAML.")
+        return
+
+    print(openapi)
+
+
+# TODO make a more precise return type definition
+def parse_yaml(filepath: str) -> Mapping[str, Any]:
+    """Parse a YAML file into a Python dictionary.
+
+    Args:
+        filepath: Path of the YAML file to be parsed.
+
+    Returns:
+        Python dictionary representing the YAML file.
+    """
+    try:
+        with open(filepath) as yaml_file:
+            try:
+                return yaml.safe_load(yaml_file)
+            except yaml.YAMLError as exc:
+                print(exc)
+    except FileNotFoundError:
+        print(f"File {filepath} not found.")
 
 
 if __name__ == "__main__":
