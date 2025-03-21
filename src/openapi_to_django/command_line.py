ea4f74ecf582214bc77047bc5a3daba13d5bd2f4
jaytik1 <67837855+jaytik1@users.noreply.github.com>
2025-03-21 12:28:04 +0000

    chore: add templates to the package
    Refs: #27

--- src/openapi_to_django/command_line.py
+++ src/openapi_to_django/command_line.py
@@ -8,13 +8,15 @@ import argparse
 from enum import Enum
 from pathlib import Path
 
-import openapi_to_django.loadopenapi
 from django.core.management import call_command
+
+import openapi_to_django.loadopenapi
 
 DEFAULT_PROJECT_NAME = "openapi_django"
 DEFAULT_APP_NAME = DEFAULT_PROJECT_NAME + "_app"
 
-TEMPLATE_DIR = Path(__file__).parent.parent / "templates"
+# TEMPLATE_DIR = Path(__file__).parent.parent / "templates"
+TEMPLATE_DIR = Path(__file__).parent / "templates"
 PROJECT_TEMPLATE_DIR = TEMPLATE_DIR / "project_template"
 APP_TEMPLATE_DIR = TEMPLATE_DIR / "app_template"
 URLS_TEMPLATE_PATH = TEMPLATE_DIR / "urls.py-tpl"
@@ -79,7 +81,7 @@ def main() -> None:
 
     # automatically load the OpenAPI document
     print(f"Loading OpenAPI document {args.openapi_file}...")
-    load_openapi_command = loadopenapi.Command()
+    load_openapi_command = openapi_to_django.loadopenapi.Command()
     call_command(
         load_openapi_command,
         args.openapi_file,
